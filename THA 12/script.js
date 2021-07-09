a1084eeaeadb1d21cef318501dbce40e3bb96425
Prasanna Anjankar <prasannaanjankar@gmail.com>
2021-07-09 09:26:19 +0530

    fu

---
+++ THA 12/script.js
@@ -0,0 +1,71 @@
+const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListbButton = document.querySelector('[data-delete-list-button]');
+
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists' ; //adding . ie namespace helps to avoidndata repitition
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListID = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+
+listsContainer.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'li'){
+       selectedListID = e.target.listId;
+       saveAndRender();
+    }
+})
+
+
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListInput.value;
+    if(listName == null || listName==='') return
+    const list = createList(listName)
+    newListInput.value = null;
+    lists.push(list);
+    saveAndRender();
+})
+function createList(name){
+    return { id: Date.now().toString() , name: name , task: []}
+}
+
+function saveAndRender(){
+    save();
+    render();
+}
+
+function save(){
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListID);
+}
+
+function render(){
+    clearElement(listsContainer);
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
+        listElement.classList.add("list-name");
+        listElement.innerText = list.name;
+        if(list.id === selectedListID){
+            listElement.classList.add('active-list');
+        }
+        listsContainer.appendChild(listElement);
+    })
+}
+
+function clearElement(element){
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+render();
+
+// 100=a1
+// 95=a
+// 90=b+
+// 85=b
+// 80=c+
+// 75
