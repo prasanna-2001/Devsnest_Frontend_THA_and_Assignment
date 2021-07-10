00e8d83af2a99ab2ae0da7453043e74ac260712d
Prasanna Anjankar <prasannaanjankar@gmail.com>
2021-07-10 22:44:29 +0530

    tha 13

--- THA 13/script.js
+++ THA 13/script.js
@@ -1,19 +1,7 @@
 const input = document.querySelector('input')
 const btn = document.querySelector('button')
 
-// async function getData(data){
-    
-//     let res = await fetch(`https://api.quotable.io/random?tags=${tag}`)
-//     let data = await res.json()
-//     return data;
-    
-// }
-// btn.addEventListener('click',async ()=>{
-//     let response = await getData(input.value);
-//     let quote = response.content;
-//     let author = response.author;
-//     document.querySelector('blockquote').innerHTML = quote + "  " + `<div>${author}</div>`;
-// })
+
 
 async function getData(username){
     const res = await fetch(`https://api.github.com/users/${username}`)
